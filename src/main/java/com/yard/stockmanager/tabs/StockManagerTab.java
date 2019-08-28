dc29c771eeb7c473b0e423a49010c20487c4d168
martinWink <53544246+martinWink@users.noreply.github.com>
2019-08-28 20:42:41 -0300

    Alterações aba stockManager

--- src/main/java/com/yard/stockmanager/tabs/StockManagerTab.java
+++ src/main/java/com/yard/stockmanager/tabs/StockManagerTab.java
@@ -1,7 +1,13 @@
 package com.yard.stockmanager.tabs;
 
+import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.scene.text.Font;
 
@@ -15,6 +21,7 @@ public class StockManagerTab extends Tab
 
     public StockManagerTab()
     {
+        super("Gerenciamento de Estoque");
         initComponents();
     }
 
@@ -31,9 +38,23 @@ public class StockManagerTab extends Tab
     {
 
         xlabel.setFont(font);
+        xlabel.setPrefHeight(250);
+        button.setPrefSize(100, 30);
+        button.setPadding(new Insets(5));
+        button.setVisible(true);
+
+        tela.setPadding(new Insets(100));
+        tela.addRow(0,xlabel,button);
+        setContent(tela);
+
+
     }
 
 
 
-    private Label xlabel = new Label("X");
+    private Label xlabel = new Label("Xaidhsniajdsaijndiasj");
+
+    private Button button = new Button("Botão adbk  akj");
+
+    private GridPane tela = new GridPane();
 }
