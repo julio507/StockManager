4177afb259ee847317231ac2db7d5db006b28bba
Heli <Heli_B@users.noreply.github.com>
2019-10-16 20:07:49 -0300

    Implementação do modulo de envio de emails
    criação de um metodo estático para o envio de emails de notificação

---
+++ src/main/java/com/yard/stockmanager/mail/Mail.java
@@ -0,0 +1,62 @@
+package com.yard.stockmanager.mail;
+
+import com.sun.mail.util.MailSSLSocketFactory;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.security.GeneralSecurityException;
+import java.util.Properties;
+
+public class Mail {
+    public static void sendMessage(Address[] destinos)
+    {
+        Properties props = new Properties();
+        /** Parâmetros de conexão com servidor Gmail */
+        MailSSLSocketFactory sf = null;
+        try {
+            sf = new MailSSLSocketFactory();
+        } catch (GeneralSecurityException e) {
+            e.printStackTrace();
+        }
+        sf.setTrustAllHosts(true);
+        //propriedades
+        props.put("mail.smtp.ssl.trust", "*");
+        props.put("mail.smtp.ssl.socketFactory", sf);
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+
+        Session session = Session.getDefaultInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication("helimar.baggattini@universo.univates.br","hygbnjuhUn15");
+                    }
+                });
+
+        /** Ativa Debug para sessão */
+        session.setDebug(true);
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("helimar.baggattini@universo.univates.br"));//Remetente
+
+            Address[] toUser = InternetAddress.parse("helimarbaggattini15@gmail.com");//destinos;//Destinatário(s)
+
+            message.setRecipients(Message.RecipientType.TO, toUser);
+            message.setSubject("Enviando email com JavaMail");//Assunto
+            message.setText("Enviei este email utilizando JavaMail com minha conta GMail !");
+            /**Método para enviar a mensagem criada*/
+            Transport.send(message);
+
+            System.out.println("Feito!!!");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
