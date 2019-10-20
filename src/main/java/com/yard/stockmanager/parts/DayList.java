80cca6a290d4e9dafa3eefea571fe05fce181a36
julio507 <julio_bruch@hotmail.com>
2019-10-20 20:22:47 -0300

    Ajustado calendario
    Ajustado para permitir seleção de datas

--- src/main/java/com/yard/stockmanager/parts/DayList.java
+++ src/main/java/com/yard/stockmanager/parts/DayList.java
@@ -11,6 +11,58 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 
 public class DayList extends BorderPane {
+    
+    public enum DayWeek
+    {
+        SUN( "Domingo" ),
+        MON( "Segunda" ),
+        TUE( "Terça" ),
+        WED( "Quarta" ),
+        THU( "Quinta" ),
+        FRI( "Sexta" ),
+        SAT( "Sabado" );
+        
+        private final String name;
+
+        DayWeek( String name )
+        {
+            this.name = name;
+        }
+
+        public String toString()
+        {
+            return name;
+        }
+    }
+
+    public enum Month
+    {
+        JAN( "Janeiro" ),
+        FEB( "Fevereiro" ),
+        MAR( "Março" ),
+        APR( "Abril" ),
+        MAY( "Maio" ),
+        JUN( "Junho" ),
+        JUL( "Julho" ),
+        AUG( "Agosto" ),
+        SEP( "Setembro" ),
+        OCT( "Outubro" ),
+        NOV( "Novembro" ),
+        DEC( "Dezembro" );
+        
+        private String name;
+
+        Month( String name )
+        {
+            this.name = name;
+        }
+
+        public String toString()
+        {
+            return name;
+        }
+    }
+
     private LocalDate date;
     private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
--- src/main/java/com/yard/stockmanager/tabs/CalendarManagementTab.java
+++ src/main/java/com/yard/stockmanager/tabs/CalendarManagementTab.java
@@ -2,10 +2,17 @@ package com.yard.stockmanager.tabs;
 
 import java.time.LocalDate;
 import java.time.temporal.WeekFields;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 
 import com.yard.stockmanager.parts.DayList;
+import com.yard.stockmanager.parts.DayList.DayWeek;
+import com.yard.stockmanager.parts.DayList.Month;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TextField;
@@ -14,72 +21,90 @@ import javafx.scene.layout.GridPane;
 
 public class CalendarManagementTab extends Tab {
 
-    private enum DayWeek
-    {
-        DOM( "Domingo" ),
-        SEG( "Segunda" ),
-        TER( "Terça" ),
-        QUA( "Quarta" ),
-        QUI( "Quinta" ),
-        SEX( "Sexta" ),
-        SAB( "Sabado" );
-        
-        private final String name;
+    private LocalDate localDate = LocalDate.now();
 
-        DayWeek( String name )
-        {
-            this.name = name;
-        }
-
-        public String toString()
-        {
-            return name;
-        }
-    }
+    private List<Month> months = Arrays.asList(Month.values());
 
     public CalendarManagementTab() {
         super("Calendario");
-        
+
         initCompoents();
 
         refreshContent();
     }
 
     private void refreshContent() {
-        
-        LocalDate localDate = LocalDate.now();
+        centerGrid.getChildren().clear();
 
-        for (int i = 1; i < localDate.lengthOfMonth() + 1; i++) 
-        {
-            LocalDate date = localDate.withDayOfMonth( i );
+        for (int i = 1; i < localDate.lengthOfMonth() + 1; i++) {
+            LocalDate date = localDate.withDayOfMonth(i);
 
-            WeekFields week = WeekFields.of( Locale.getDefault() );
+            WeekFields week = WeekFields.of(Locale.getDefault());
 
-            centerGrid.add( new DayList( date ), date.get( week.dayOfWeek() ), date.get( week.weekOfWeekBasedYear() ) );
+            centerGrid.add(new DayList(date), date.get(week.dayOfWeek()), date.get(week.weekOfWeekBasedYear()));
         }
 
         for (DayWeek d : DayWeek.values()) {
             TextField field = new TextField();
 
-            field.setText( d.toString() );
-            field.setEditable( false );
+            field.setText(d.toString());
+            field.setEditable(false);
             centerGrid.add(field, d.ordinal() + 1, 0);
         }
+
+        monthCombo.getSelectionModel().select(localDate.getMonthValue() - 1);
+        yearCombo.getSelectionModel().select((Integer) localDate.getYear());
     }
 
     public void initCompoents() {
-        centerGrid.setGridLinesVisible(true);
 
-        borderpane.setTop( topGrid );
+        for (int i = localDate.getYear() - 20; i < localDate.getYear(); i++) {
+            yearCombo.getItems().add(i);
+        }
+
+        for (int i = localDate.getYear(); i < localDate.getYear() + 20; i++) {
+            yearCombo.getItems().add(i);
+        }
+
+        monthCombo.getItems().addAll(months);
+
+        topGrid.addRow(0, monthLabel, monthCombo, yearLabel, yearCombo);
+
+        borderpane.setTop(topGrid);
         borderpane.setCenter(centerGrid);
         setContent(borderpane);
+
+        monthCombo.valueProperty().addListener(new ChangeListener<Month>() {
+
+            @Override
+            public void changed(ObservableValue<? extends Month> observable, Month oldValue, Month newValue) {
+                localDate = localDate.withMonth(newValue.ordinal() + 1);
+
+                refreshContent();
+            }
+
+        });
+
+        yearCombo.valueProperty().addListener(new ChangeListener<Integer>() {
+
+            @Override
+            public void changed(ObservableValue<? extends Integer> observable, Integer oldValue, Integer newValue) {
+                localDate = localDate.withYear(newValue);
+
+                refreshContent();
+            }
+
+        });
     }
 
-    private Label monthLabel = new Label( "Mes" );
-    private Label yearLabel = new Label( "Ano" );
+    private Label monthLabel = new Label("Mes:");
+    private Label yearLabel = new Label("Ano:");
+
+    private ComboBox monthCombo = new ComboBox<Month>();
+    private ComboBox yearCombo = new ComboBox<Integer>();
 
     private GridPane topGrid = new GridPane();
-    
+
     private GridPane centerGrid = new GridPane();
     private BorderPane borderpane = new BorderPane();
 }
