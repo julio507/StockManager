da0182b4f23048c28a031f02e277c6ef1cd202e2
julio507 <julio_bruch@hotmail.com>
2019-10-23 00:14:53 -0300

    Ajuste script de teste

--- db/teste.sql
+++ db/teste.sql
@@ -1,10 +1,11 @@
-insert into funcionario values( DEFAULT, 1, 'adm', md5('warlock'), 'mestre dos magos', '1','teste@testando.com', 'dunno', '1' );
 
-insert into pessoa values ( default, 1, 'Sr', 'Junho', '666', null, '123', '123', '1' );
+insert into cidade values ( default, 'Lajeado', 'RS', '1' ); 
 
 insert into endereco values ( default, 1,'unknown', 'unknown', '42', null , '123', NULL, '1' );
 
-insert into cidade values ( default, 'Lajeado', 'RS', '1' ); 
+insert into pessoa values ( default, 1, 'Sr', 'Junho', '666', null, '123', '123', '1' );
+
+insert into funcionario values( DEFAULT, 1, 'adm', md5('warlock'), 'mestre dos magos', '1','teste@testando.com', 'dunno', '1' );
 
 select * from funcionario;
 
