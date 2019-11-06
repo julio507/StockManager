0a05b706c30222eebda41ac7d07026ac8205df17
Heli <Heli_B@users.noreply.github.com>
2019-11-06 19:54:35 -0300

    Criação da classe ProdutoDAO

---
+++ src/main/java/com/yard/stockmanager/persistence/dao/ProdutoDAO.java
@@ -0,0 +1,35 @@
+package com.yard.stockmanager.persistence.dao;
+
+import com.yard.stockmanager.persistence.entity.Produto;
+import com.yard.stockmanager.persistence.hibernate.HibernateUtil;
+import org.hibernate.Session;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProdutoDAO extends
+        Dao<Produto> {
+    public static List<Produto> getAll(String busca) {
+
+        List<Produto> list;
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        s.beginTransaction();
+        list = s.createQuery("select new Produto (p.id, p.categoria, p.departamento, p.marca, p.unidade, p.nome, p.descricao, p.quantidade, p.custounitario, p.ativo) " +
+                "FROM Produto p, Categoria c, Departamento d, Marca m, Unidade u WHERE p.categoria = c.id and p.departamento = d.id " +
+                "and p.marca = m.id and p.unidade = u.id and p.nome like '%" + busca + "%'").list();
+
+        //inicialização das classes auxiliares
+        for (Produto p : list)
+        {
+            p.getId();
+            p.getCategoria().toString();
+            p.getDepartamento().toString();
+            p.getMarca().toString();
+            p.getUnidade().toString();
+        }
+        s.getTransaction().commit();
+        s.close();
+
+        return list;
+    }
+}
