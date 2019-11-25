77b7853aca421810156253175b730c28d040eb43
EduardoBack7 <50850584+EduardoBack7@users.noreply.github.com>
2019-11-24 23:21:04 -0300

    Update PeopleRegisterTab.java

--- src/main/java/com/yard/stockmanager/tabs/PeopleRegisterTab.java
+++ src/main/java/com/yard/stockmanager/tabs/PeopleRegisterTab.java
@@ -9,6 +9,10 @@ import javafx.collections.FXCollections;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 
 import javax.swing.*;
 import java.util.List;
@@ -71,20 +75,28 @@ public class PeopleRegisterTab extends ManagementTab<Pessoa> {
     public void save() {
         Pessoa pes = new Pessoa();
         Endereco end = new Endereco();
-        Telefones tel = new Telefones();
+        end.setEndereco(tfdEndereco.getText());
+        end.setRua(tfdRua.getText());
+        end.setNumero(tfdNumero.getText());
+        end.setCep(tfdCep.getText());
+        end.setBairro(tfdBairro.getText());
+        end.setAtivo(tfdAtivo1.getText().charAt(0));
 
-        pes.setId(1);
-        pes.setDenominacaosocial(tfdDenominacaoSocial.getText());
+
+        pes.setEndereco(end);
+        pes.setDenominacaosocial(tfdId.getText());
         pes.setNome(tfdNome.getText());
         pes.setEmail(tfdEmail.getText());
         pes.setCnpj(tfdCnpj.getText());
         pes.setCpf(tfdCpf.getText());
         pes.setObservacoes(tfdObservacoes.getText());
-        pes.setEndereco(end);
-//        falta combobox dos telefones/tipos
+        pes.setAtivo(tfdAtivo.getText().charAt(0));
+
 
         PeopleRegisterDAO pesDao = new PeopleRegisterDAO();
         pesDao.add(pes);
+        System.out.println("Salvou");
+        refresh();
     }
 
     @Override
@@ -117,26 +129,8 @@ public class PeopleRegisterTab extends ManagementTab<Pessoa> {
 
     @Override
     public void select() {
-        selected = (Pessoa) getSelected();
 
-        if (selected != null) {
-            tfdDenominacaoSocial.setText(selected.getDenominacaosocial());
-            tfdNome.setText(selected.getNome());
-            tfdEmail.setText(selected.getEmail());
-            tfdCnpj.setText(selected.getCnpj());
-            tfdCpf.setText(selected.getCpf());
-            tfdObservacoes.setText(selected.getObservacoes());
-            id = selected.getId();
-            // falta telefone/tipo
-        }
-        else
-            {
-            clear();
-        }
     }
-
-
-
 
     @Override
     public void clear() {
@@ -144,17 +138,32 @@ public class PeopleRegisterTab extends ManagementTab<Pessoa> {
     }
 
     private void initComponents() {
+        txtTitlePess.setFont(Font.font("System", FontWeight.BOLD, 20));
+        txtTitleEnd.setFont(Font.font("System", FontWeight.BOLD, 20));
+
+        innerGrid.addRow(0, hbxTitlePess);
+        innerGrid.addRow(1,labCodigo, tfdCodigo);
+        innerGrid.addRow(2,labDenominacaoSocial, tfdDenominacaoSocial);
+        innerGrid.addRow(3,labNome, tfdNome);
+        innerGrid.addRow(4,labTelefone, tfdTelefone);
+        innerGrid.addRow(5,labEmail, tfdEmail);
+        innerGrid.addRow(6,labCnpj, tfdCnpj);
+        innerGrid.addRow(7,labCpf, tfdCpf);
+        innerGrid.addRow(8,labObservacoes, tfdObservacoes);
+        innerGrid.addRow(9,labAtivo, tfdAtivo);
+        innerGrid.addRow(11, hbxTitleEnd);
+        innerGrid.addRow(12,labId, tfdId);
+        innerGrid.addRow(13,labCidade, tfdCidade);
+        innerGrid.addRow(14,labEndereco, tfdEndereco);
+        innerGrid.addRow(15,labRua, tfdRua);
+        innerGrid.addRow(16,labCEP, tfdCep);
+        innerGrid.addRow(17,labAtivo1, tfdAtivo1);
+        innerGrid.addRow(18, labBairro, tfdBairro);
 
 
-        innerGrid.addRow(0,labCodigo, tfdCodigo);
-        innerGrid.addRow(1,labDenominacaoSocial, tfdDenominacaoSocial);
-        innerGrid.addRow(2,labNome, tfdNome);
-        innerGrid.addRow(3,labTelefone, tfdTelefone);
-        innerGrid.addRow(4,labEmail, tfdEmail);
-        innerGrid.addRow(5,labCnpj, tfdCnpj);
-        innerGrid.addRow(6,labCpf, tfdCpf);
-        innerGrid.addRow(7,labObservacoes, tfdObservacoes);
-        innerGrid.addRow(8,labAtivo, tfdAtivo);
+
+
+
 
 
         TableColumn<Pessoa, Integer> id = new TableColumn<>("Código");
@@ -185,7 +194,31 @@ public class PeopleRegisterTab extends ManagementTab<Pessoa> {
     private int id;
 
     //Criação dos componentes da tela
+    private Text txtTitleEnd = new Text("Dados do Endereço");
+    private HBox hbxTitleEnd = new HBox(txtTitleEnd);
 
+
+    private Label labId = new Label ("ID");
+    private Label labCidade = new Label ("Cidade:");
+    private Label labEndereco = new Label ("Endereço:");
+    private Label labRua = new Label ("Rua:");
+    private Label labNumero = new Label ("Número:");
+    private Label labCEP = new Label ("CEP:");
+    private Label labAtivo1 = new Label ("Ativo:");
+    private Label labBairro = new Label ("Bairro:");
+
+
+    private TextField tfdId = new TextField();
+    private TextField tfdCidade = new TextField();
+    private TextField tfdEndereco = new TextField();
+    private TextField tfdRua = new TextField();
+    private TextField tfdNumero = new TextField();
+    private TextField tfdCep = new TextField();
+    private TextField tfdAtivo1 = new TextField();
+    private TextField tfdBairro = new TextField();
+
+    private Text txtTitlePess = new Text("Dados da Pessoa");
+    private HBox hbxTitlePess = new HBox(txtTitlePess);
     private Label labCodigo = new Label("Código:");
     private Label labDenominacaoSocial = new Label("Denominação Social");
     private Label labNome = new Label("Nome:");
@@ -207,4 +240,19 @@ public class PeopleRegisterTab extends ManagementTab<Pessoa> {
     private TextField tfdObservacoes = new TextField();
     private TextField tfdAtivo = new TextField();
 
+    public HBox getHbxTitleEnd() {
+        return hbxTitleEnd;
+    }
+
+    public void setHbxTitleEnd(HBox hbxTitleEnd) {
+        this.hbxTitleEnd = hbxTitleEnd;
+    }
+
+    public HBox getHbxTitlePess() {
+        return hbxTitlePess;
+    }
+
+    public void setHbxTitlePess(HBox hbxTitlePess) {
+        this.hbxTitlePess = hbxTitlePess;
+    }
 }
