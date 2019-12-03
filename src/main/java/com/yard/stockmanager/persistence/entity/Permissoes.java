84739d0705574aba336085d9f04fa48a2304b42d
Heli <Heli_B@users.noreply.github.com>
2019-12-02 22:53:43 -0300

    Criação da entidade Permissoes
    Criação da entidade e do mapeamento

---
+++ src/main/java/com/yard/stockmanager/persistence/entity/Permissoes.java
@@ -0,0 +1,49 @@
+package com.yard.stockmanager.persistence.entity;
+
+import javax.persistence.*;
+
+public class Permissoes {
+    private PermissoesId id;
+    private Funcionario funcionario;
+    private String regra;
+
+    public Permissoes() {
+    }
+
+    public Permissoes(Funcionario funcionario, String regra) {
+        this.funcionario = funcionario;
+        this.regra = regra;
+    }
+
+    @EmbeddedId
+    @AttributeOverrides( {
+            @AttributeOverride(name="funcionarioId", column=@Column(name="funcionario_id", nullable=false) ) } )
+    public PermissoesId getId() {
+        return id;
+    }
+
+    public void setId(PermissoesId id) {
+        this.id = id;
+    }
+
+    @ManyToOne(fetch= FetchType.LAZY)
+    @JoinColumn(name="funcionario_id", nullable=false)
+    public Funcionario getFuncionario() {
+        return funcionario;
+    }
+
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
+    }
+
+    @Column(name="regra", nullable=false, length=100)
+    public String getRegra() {
+        return regra;
+    }
+
+    public void setRegra(String regra) {
+        this.regra = regra;
+    }
+
+
+}
